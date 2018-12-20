7a0233d9208989631697c555047de42cae0ebccf
Harry Kim <hkim@securitycompass.com>
2018-12-20 15:08:43 -0500

    fix

--- yantra/__init__.py
+++ yantra/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import absolute_import
 
-from .manager import PluginManager, PluginType  # noqa
+from .manager import PluginManager, PluginType
+
+__all__ = [PluginManager, PluginType]
